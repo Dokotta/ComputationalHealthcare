3cd9d4a9daba608e5deb6e07f555d655f384f3ab
akshay bhat <akshayubhat@gmail.com>
2017-01-11 23:31:51 -0500

    Implementing simple UI

--- CH/settings.py
+++ CH/settings.py
@@ -15,7 +15,7 @@ import os, sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BROKER_URL = 'amqp://{}:{}@localhost//'.format('rabbit','rabbit')
+BROKER_URL = 'amqp://{}:{}@localhost//'.format('chuser','chpass')
 
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
